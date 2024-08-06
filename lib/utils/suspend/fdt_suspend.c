27ca4ebf776c5f8acfb9c4e8c02563aadda83c43
Anup Patel <apatel@ventanamicro.com>
2024-08-06 10:27:42 +0530

    lib: utils: Add simple FDT based system suspend driver framework
    The generic platform can have multiple system suspend drivers so add a
    simple FDT based system suspend driver framework.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>

---
+++ lib/utils/suspend/fdt_suspend.c
@@ -0,0 +1,45 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024 Ventana Micro Systems Inc.
+ *
+ * Authors:
+ *   Anup Patel <apatel@ventanamicro.com>
+ */
+
+#include <sbi/sbi_console.h>
+#include <sbi/sbi_error.h>
+#include <sbi_utils/fdt/fdt_helper.h>
+#include <sbi_utils/suspend/fdt_suspend.h>
+
+/* List of FDT suspend drivers generated at compile time */
+extern struct fdt_suspend *fdt_suspend_drivers[];
+extern unsigned long fdt_suspend_drivers_size;
+
+int fdt_suspend_driver_init(const void *fdt, struct fdt_suspend *drv)
+{
+	int noff, rc = SBI_ENODEV;
+	const struct fdt_match *match;
+
+	noff = fdt_find_match(fdt, -1, drv->match_table, &match);
+	if (noff < 0)
+		return SBI_ENODEV;
+
+	if (drv->init) {
+		rc = drv->init(fdt, noff, match);
+		if (rc && rc != SBI_ENODEV) {
+			sbi_printf("%s: %s init failed, %d\n",
+				   __func__, match->compatible, rc);
+		}
+	}
+
+	return rc;
+}
+
+void fdt_suspend_init(const void *fdt)
+{
+	int pos;
+
+	for (pos = 0; pos < fdt_suspend_drivers_size; pos++)
+		fdt_suspend_driver_init(fdt, fdt_suspend_drivers[pos]);
+}
